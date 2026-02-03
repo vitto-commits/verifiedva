9e91b6a5178d4777d27696e150eb0b0c8790ad9e
vitto-commits <vitto@verticestaffing.com>
2026-02-03 20:01:05 +0000

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Search from './pages/Search'
 import VAProfile from './pages/VAProfile'
@@ -9,7 +9,7 @@ import Dashboard from './pages/Dashboard'
 function App() {
   return (
     <div className="dark bg-gray-950 text-gray-100 min-h-screen">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
@@ -18,7 +18,7 @@ function App() {
           <Route path="/client/signup" element={<ClientSignup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
